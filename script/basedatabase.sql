e51640ca72af9d584491b4538a5202dfb04b21f3
PC <praveenchaturvedi@gmail.com>
2018-01-28 14:02:44 +0530

    DB Script

---
+++ script/basedatabase.sql
@@ -0,0 +1,242 @@
+CREATE DATABASE  IF NOT EXISTS `myeschool` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `myeschool`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: myeschool
+-- ------------------------------------------------------
+-- Server version	5.7.20-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `articles`
+--
+
+DROP TABLE IF EXISTS `articles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `articles` (
+  `article_id` int(5) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) NOT NULL,
+  `category` varchar(100) NOT NULL,
+  PRIMARY KEY (`article_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `articles`
+--
+
+LOCK TABLES `articles` WRITE;
+/*!40000 ALTER TABLE `articles` DISABLE KEYS */;
+INSERT INTO `articles` VALUES (1,'Java Concurrency','Java'),(2,'Hibernate HQL ','Hibernate'),(3,'Spring MVC with Hibernate','Spring');
+/*!40000 ALTER TABLE `articles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'Admin');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `spring_session`
+--
+
+DROP TABLE IF EXISTS `spring_session`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `spring_session` (
+  `SESSION_ID` char(36) NOT NULL,
+  `CREATION_TIME` bigint(20) NOT NULL,
+  `LAST_ACCESS_TIME` bigint(20) NOT NULL,
+  `MAX_INACTIVE_INTERVAL` int(11) NOT NULL,
+  `PRINCIPAL_NAME` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`SESSION_ID`),
+  KEY `SPRING_SESSION_IX1` (`LAST_ACCESS_TIME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `spring_session`
+--
+
+LOCK TABLES `spring_session` WRITE;
+/*!40000 ALTER TABLE `spring_session` DISABLE KEYS */;
+/*!40000 ALTER TABLE `spring_session` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `spring_session_attributes`
+--
+
+DROP TABLE IF EXISTS `spring_session_attributes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `spring_session_attributes` (
+  `SESSION_ID` char(36) NOT NULL,
+  `ATTRIBUTE_NAME` varchar(200) NOT NULL,
+  `ATTRIBUTE_BYTES` blob NOT NULL,
+  PRIMARY KEY (`SESSION_ID`,`ATTRIBUTE_NAME`),
+  KEY `SPRING_SESSION_ATTRIBUTES_IX1` (`SESSION_ID`),
+  CONSTRAINT `SPRING_SESSION_ATTRIBUTES_FK` FOREIGN KEY (`SESSION_ID`) REFERENCES `spring_session` (`SESSION_ID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `spring_session_attributes`
+--
+
+LOCK TABLES `spring_session_attributes` WRITE;
+/*!40000 ALTER TABLE `spring_session_attributes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `spring_session_attributes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task_list`
+--
+
+DROP TABLE IF EXISTS `task_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task_list` (
+  `task_id` int(11) NOT NULL AUTO_INCREMENT,
+  `task_name` varchar(100) NOT NULL,
+  `task_description` text,
+  `task_priority` varchar(20) DEFAULT NULL,
+  `task_status` varchar(20) DEFAULT NULL,
+  `task_start_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `task_end_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `task_archived` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`task_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task_list`
+--
+
+LOCK TABLES `task_list` WRITE;
+/*!40000 ALTER TABLE `task_list` DISABLE KEYS */;
+INSERT INTO `task_list` VALUES (1,'Gathering Requirement','Requirement Gathering','MEDIUM','ACTIVE','2018-01-01 14:23:51','2018-01-01 17:23:51',0),(2,'Application Designing','Application Designing','MEDIUM','ACTIVE','2018-01-01 14:23:51','2018-01-01 16:23:51',0),(3,'Implementation','Implementation','MEDIUM','ACTIVE','2018-01-01 14:23:51','2018-01-01 17:23:51',0),(4,'Unit Testing','Unit Testing','LOW','ACTIVE','2018-01-01 14:23:51','2018-01-01 18:23:51',0),(5,'Maintanence','Maintanence','LOW','ACTIVE','2018-01-01 14:23:51','2018-01-01 19:23:51',0);
+/*!40000 ALTER TABLE `task_list` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` text,
+  `passwordConfirm` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'PraveenC','$2a$10$yGKr2mF7af.eB4jISfcxdu5OPJ0tQU5GE/j1WDSiGBDF1DCwfaLqu',NULL),(2,'PraveenC1','$2a$10$75N30wvXkrxLF0zOq4.cuOv0XEPStUK6xbyCvzjnJKorNnTPjaldu',NULL),(3,'SunilK','$2a$10$/FFsX7SWqHJAob3HNFTzsua2KMTcbQ.cBilRkPmoofY9Ss64EtQk2',NULL),(4,'Mukesh','$2a$10$8iUrn337XftmOhwT8kkhPO8WH3TaIOtcZIqX15tZpFTfkY72ctapW',NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_role`
+--
+
+DROP TABLE IF EXISTS `user_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `user_role_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `user_role_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_role`
+--
+
+LOCK TABLES `user_role` WRITE;
+/*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
+INSERT INTO `user_role` VALUES (1,2,1),(2,3,1),(3,4,1);
+/*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `USER_ID` int(11) NOT NULL,
+  `LOGIN_ID` varchar(50) DEFAULT NULL,
+  `PASSWORD` text,
+  `USER_LEVEL` int(2) DEFAULT NULL,
+  PRIMARY KEY (`USER_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'pc@gmail.com','fran123',1),(2,'sc@gmail.com','fran123',2);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-01-28 14:01:18
